6dc88d4565346e744a6fd169849dabfb4a8aacc8
Craig Williams <cwilliams.allancraig@gmail.com>
2012-09-29 23:08:16 -0600

    Moved shared examples into file since error finding file during Travis build

--- spec/tobacco_spec.rb
+++ spec/tobacco_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
+shared_examples_for "it's configured" do
+  it 'sets the publish host' do
+    Tobacco.published_host.should == published_host
+  end
+
+  it 'sets the base path' do
+    Tobacco.base_path.should == base_path
+  end
+
+  it 'sets the content method' do
+    Tobacco.content_method.should == content_method
+  end
+
+  it 'sets the content url method' do
+    Tobacco.content_url_method.should == content_url_method
+  end
+
+  it 'sets the output filepath method' do
+    Tobacco.output_filepath_method.should == output_filepath_method
+  end
+end
+
+
 describe Tobacco do
 
   context 'configuration' do
@@ -21,7 +44,7 @@ describe Tobacco do
         end
       end
 
-      it_behaves_like "it is configured"
+      it_behaves_like "it's configured"
     end
 
     context 'configuration options override defaults' do
@@ -41,7 +64,7 @@ describe Tobacco do
         end
       end
 
-      it_behaves_like "it is configured"
+      it_behaves_like "it's configured"
     end
   end
 end
