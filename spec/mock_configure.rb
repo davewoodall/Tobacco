b1920f823e802bccc9f31d6c6326156c94a43b0d
Craig Williams <cwilliams.allancraig@gmail.com>
2012-09-29 22:11:26 -0600

    Remove unused files

--- spec/mock_configure.rb
+++
@@ -1,7 +0,0 @@
-Tobacco.configure do |config|
-  config.published_host         = 'http://localhost:3000'
-  config.base_path              = 'published_content'
-  config.content_method         = :content
-  config.content_url_method     = :content_url
-  config.output_filepath_method = :output_filepath
-end
