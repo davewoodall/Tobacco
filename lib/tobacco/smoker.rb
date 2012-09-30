1661cf246bd9939c80d1471315772eabbfa9aa77
Craig Williams <cwilliams.allancraig@gmail.com>
2012-09-30 12:19:25 -0600

    Adding more specs to Tobacco::Smoker

--- lib/tobacco/smoker.rb
+++ lib/tobacco/smoker.rb
@@ -1,14 +1,21 @@
 module Tobacco
+  class MissingContentError < RuntimeError
+    def message
+      "No error encountered but content is empty"
+    end
+  end
+
   class Smoker
 
     attr_accessor :smoker,
       :file_path_generator,
       :reader,
-      :writer
+      :writer,
+      :content
 
-    def initialize(smoker)
-      self.smoker = smoker
-      @content    = nil
+    def initialize(smoker, content = '')
+      self.smoker  = smoker
+      self.content = content
     end
 
     def generate_file_paths
@@ -16,13 +23,9 @@ module Tobacco
     end
 
     def read
-      self.reader = choose_reader
-      @content    = reader.send(Tobacco.content_method)
-
-      unless content_present?
-        error = error_object('Error Reading', '', @content)
-        callback(:on_read_error, )
-      end
+      choose_reader
+      read_content
+      verify_content
     end
 
     def write!
@@ -45,7 +48,74 @@ module Tobacco
     end
 
     #---------------------------------------------------------
-    private
+    # Write helper methods
+    #---------------------------------------------------------
+    def modify_content_before_writing
+      callback(:before_write, content)
+    end
+
+    #---------------------------------------------------------
+    # Read helper methods
+    #---------------------------------------------------------
+    def read_content
+      self.content = reader.send(Tobacco.content_method)
+    end
+
+    def verify_content
+      unless content_present?
+
+        # At this point, the content might be an error object
+        # but if not, we create one
+        #
+        object = missing_content_error(content)
+        error  = error_object('Error Reading', '', object)
+
+        callback(:on_read_error, error)
+      end
+    end
+
+    def missing_content_error(content)
+      if content.respond_to? :message
+        content
+      else
+        Tobacco::MissingContentError.new
+      end
+    end
+
+    def content_present?
+      @content_present ||= content?
+    end
+
+    def content?
+      return false if content.nil? || content.empty?
+
+      Array(content).last !~ /404 Not Found|The page you were looking for doesn't exist/
+    end
+
+    def choose_reader
+      # The reader will either be the calling class (smoker)
+      # if it provides the content method or a new Inhaler
+      # object that will be used to read the content from a url
+      #
+      self.reader = \
+        if smoker.respond_to? Tobacco.content_method
+          smoker
+        else
+          Inhaler.new(file_path_generator.content_url).tap do |inhaler|
+
+            # Add an alias for the user configured content_method
+            # so that when it is called it calls :read
+            # on the Inhaler instance
+            #
+            inhaler.instance_eval %{
+              alias :"#{Tobacco.content_method}" :read
+            }
+          end
+      end
+    end
+
+    #---------------------------------------------------------
+    # private
 
     def error_object(msg, modified_content, e)
       Tobacco::Error.new(
@@ -56,40 +126,9 @@ module Tobacco
       )
     end
 
-    def modify_content_before_writing
-      callback(:before_write, @content)
-    end
-
-    def content_present?
-      @content_present ||= content?
-    end
-
-    def content?
-      return false if @content.nil? || @content.empty?
-
-      Array(@content).last !~ /404 Not Found|The page you were looking for doesn't exist/
-    end
-
-    def choose_reader
-      if smoker.respond_to? Tobacco.content_method
-        smoker
-      else
-        Inhaler.new(file_path_generator.content_url).tap do |inhaler|
-
-          # Add an alias for the user configured content_method
-          # so that when it is called it calls fetch_content
-          # on the Inhaler instance
-          #
-          inhaler.instance_eval %{
-            alias :"#{Tobacco.content_method}" :read
-          }
-        end
-      end
-    end
-
-    def callback(name, *args)
+    def callback(name, error)
       if smoker.respond_to? name
-        smoker.send(name, args)
+        smoker.send(name, error)
       end
     end
   end
--- spec/tobacco/smoker_spec.rb
+++ spec/tobacco/smoker_spec.rb
@@ -2,6 +2,125 @@ require 'spec_helper'
 require 'tobacco'
 
 describe Tobacco::Smoker do
+  let(:smoker) { mock('smoker') }
 
+  subject { Tobacco::Smoker.new(smoker) }
 
+  context '#write!' do
+    context 'providing content directly' do
+
+    end
+
+    context 'providing content through the content method' do
+
+    end
+
+    context 'reading content from content_url' do
+
+    end
+  end
+
+  describe '#generate_file_paths' do
+    it 'sets the file_path_generator' do
+      Tobacco::Roller.should_receive(:new).with(smoker)
+
+      subject.generate_file_paths
+    end
+  end
+
+  describe '#choose_reader' do
+    context 'when smoker provides the content' do
+      before { smoker.stub(:content) }
+
+      it 'uses the smoker for the content' do
+        subject.choose_reader
+        subject.reader.should == smoker
+      end
+    end
+
+    context 'when smoker does not provide content' do
+      let(:inhaler)  { mock('inhaler', read: 'reader method') }
+      let(:filepath) { mock('roller', content_url: '/video') }
+
+      before do
+        Tobacco::Inhaler.should_receive(:new).and_return(inhaler)
+        subject.file_path_generator = filepath
+      end
+
+      it 'uses the smoker for the content' do
+        subject.choose_reader
+        subject.reader.should == inhaler
+      end
+    end
+  end
+
+  describe '#read_content' do
+    context 'when content is read from the smoker' do
+      let(:content) { 'Provided content' }
+
+      before { smoker.stub(:content).and_return(content) }
+
+      it 'should have the correct content' do
+        subject.choose_reader
+        subject.read_content
+
+        subject.content.should == content
+      end
+    end
+
+    context 'when content is read from url' do
+      let(:content)  { 'Content from reading url' }
+      let(:inhaler)  { mock('inhaler', read: content) }
+      let(:filepath) { mock('roller', content_url: '/video') }
+
+      before do
+        Tobacco::Inhaler.should_receive(:new).and_return(inhaler)
+        subject.file_path_generator = filepath
+      end
+
+      it 'should have the correct content' do
+        subject.choose_reader
+        subject.read_content
+
+        subject.content.should == content
+      end
+    end
+  end
+
+  describe '#read' do
+    context 'when content is empty' do
+      class Writer
+        attr_accessor :error
+        def on_read_error(error)
+          self.error = error
+        end
+      end
+
+      describe '#on_read_error' do
+        let(:inhaler)  { mock('inhaler', read: '') }
+        let(:filepath) { mock('roller', content_url: '/video', filepath: '/file/path') }
+        let(:smoker)   { Writer.new }
+
+        before do
+          Tobacco::Inhaler.should_receive(:new).and_return(inhaler)
+          subject.file_path_generator = filepath
+        end
+
+        it 'an error is returned to the callback' do
+          subject.smoker.should_receive(:on_read_error)
+
+          subject.read
+        end
+
+        it 'has the correct message on the error object' do
+          subject.read
+
+          error   = subject.smoker.error
+          message = error.object.message
+
+          message.should match(/No error encountered/)
+        end
+      end
+    end
+  end
 end
