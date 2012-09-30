0589b067049f2560b63db6b277289978a25e332a
Craig Williams <cwilliams.allancraig@gmail.com>
2012-09-29 22:12:17 -0600

    Burnout should rescue from all error so that it
    can run the requested number of times

--- lib/tobacco/burnout.rb
+++ lib/tobacco/burnout.rb
@@ -4,7 +4,7 @@ module Tobacco
     class MaximumAttemptsExceeded < Exception; end
 
     def self.try(max_attempts, &block)
-      attempts = 0
+      attempts = 1
 
       while(attempts <= max_attempts)
         success, return_value = attempt &block
@@ -12,13 +12,14 @@ module Tobacco
 
         attempts += 1
       end
+
       raise MaximumAttemptsExceeded.new("Execution timed out more than #{max_attempts} time(s).  I give up.")
     end
 
     def self.attempt(&block)
       begin
         [ true, yield ]
-      rescue Timeout::Error
+      rescue
         false
       end
     end
